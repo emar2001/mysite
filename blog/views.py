d637adafad9106fd1e8a189ca99777746bb2a031
emar2001 <70277697+emar2001@users.noreply.github.com>
2025-05-17 19:49:55 +0330

    set url parameters to manual
    made url parameters to be possible to be inserted manualy.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 def blog_view(request):
     posts = Post.objects.filter(status = 1)
@@ -8,7 +8,8 @@ def blog_view(request):
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
 
-def test(request):
-    posts = Post.objects.all()
-    context = {'posts' : posts}
+def test(request,pid):
+    #posts = Post.objects.get(id = pid)
+    posts = get_object_or_404(Post,pk = pid)
+    context = {'post' : posts}
     return render(request, 'test.html', context)

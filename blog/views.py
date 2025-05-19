cf5cd7c7071b2f5bcb1ff19930658c01722968df
emar2001 <70277697+emar2001@users.noreply.github.com>
2025-05-19 14:55:34 +0330

    Set view counter and post date
    Now posts will be only shown if their published date is past and by viewing each post, its view counts will increase by 1.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from blog.models import Post
 def blog_view(request):
-    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.counted_views += 1
+    post.save()
+    context = {'post':Post, 'pk':pk}
+    return render(request, 'blog/blog-single.html', context)
 
 def test(request,pid):
     #posts = Post.objects.get(id = pid)
